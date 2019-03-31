6001886a5e7a1495ee2c6c019e60034b59dc1ebf
Florian Völker <Neirolf2030@web.de>
2019-03-31 22:05:40 +0200

    added npm pack step and updating version

--- update-package-version.js
+++ update-package-version.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
-const fc = fs.readFileSync('./package.json', 'utf8');
-console.log('current content\n' + fs);
+
+const filePath = './package.json';
+const encooding = 'utf8';
+
+const fc = fs.readFileSync(filePath, encoding);
 const currentPackageDefinition = JSON.parse(fc);
 console.log('current version', currentPackageDefinition.version);
 console.log('tag version', process.env.TRAVIS_TAG);
+currentPackageDefinition.version = process.env.TRAVIS_TAG;
+fs.writeFileSync(filePath, JSON.stringify(currentPackageDefinition, undefined, 4), encoding);
